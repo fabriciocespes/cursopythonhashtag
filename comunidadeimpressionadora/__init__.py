f8e6241d7b20ce76f30d34e59eb513e0707d7270
fabriciocespes <125491888+fabriciocespes@users.noreply.github.com>
2023-02-18 19:14:03 -0300

    Update __init__.py

--- comunidadeimpressionadora/__init__.py
+++ comunidadeimpressionadora/__init__.py
@@ -14,9 +14,6 @@ if os.getenv("DATABASE_URL"):
     app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv("DATABASE_URL")
 else:
     app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///comunidade.db'
-#app.config['SQLALCHEMY_DATABASE_URI'] = "postgres://comunidade_dbj7_user:ud9ADfi1mCioBDCnNtvBg53fotcFntf5@dpg-cfoh9e14rebfdaopa3jg-a.oregon-postgres.render.com/comunidade_dbj7"
-
-
 
 database = SQLAlchemy(app)
 bcrypt = Bcrypt(app)
@@ -26,14 +23,14 @@ login_manager.login_message_category = 'alert-info'
 
 from comunidadeimpressionadora import models
 #
-engine = sqlalchemy.create_engine(app.config['SQLALCHEMY_DATABASE_URI'])
+#engine = sqlalchemy.create_engine(app.config['SQLALCHEMY_DATABASE_URI'])
 #
-if not engine.has_table("usuario"):
-    with app.app_context():
-        database.drop_all()
-        database.create_all()
-        print('Base de Dados Criada')
-else:
-    print('Base de Dados Existente')
+#if not engine.has_table("usuario"):
+    #with app.app_context():
+database.drop_all()
+database.create_all()
+print('Base de Dados Criada')
+#else:
+    #print('Base de Dados Existente')
 
 from comunidadeimpressionadora import routes
