981263e276d1fd9a1ebc39b4305d1169a9721c35
fabriciocespes <125491888+fabriciocespes@users.noreply.github.com>
2023-02-18 15:06:58 -0300

    Update __init__.py

--- comunidadeimpressionadora/__init__.py
+++ comunidadeimpressionadora/__init__.py
@@ -10,7 +10,13 @@ app = Flask(__name__)
 
 app.config['SECRET_KEY'] = '691f1580414a0bfc0525ad6d32f53563exit'
 
-app.config['SQLALCHEMY_DATABASE_URI'] = 'postgres://comunidade_user:6XhFUjPGwXARH8jR5Gmio0jsGzM0UJGA@dpg-cfn82tirrk0eqlv60300-a.oregon-postgres.render.com/comunidade'
+# if os.getenv("DATABASE_URL"):
+#     app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv("DATABASE_URL")
+# else:
+#     app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///comunidade.db'
+app.config['SQLALCHEMY_DATABASE_URI'] = "postgres://comunidade_ktu9_user:HFDdVcjFSvieNvhnB2fHFtQMCl21y0jz@dpg-cfo24birrk0239gabavg-a.oregon-postgres.render.com/comunidade_ktu9"
+# postgres://comunidade_ktu9_user:HFDdVcjFSvieNvhnB2fHFtQMCl21y0jz@dpg-cfo24birrk0239gabavg-a.oregon-postgres.render.com/comunidade_ktu9
+
 
 database = SQLAlchemy(app)
 bcrypt = Bcrypt(app)
@@ -18,14 +24,16 @@ login_manager = LoginManager(app)
 login_manager.login_view = 'login'
 login_manager.login_message_category = 'alert-info'
 
-from comunidadeimpressionadora import models
-engine = sqlalchemy.create_engine(app.config['SQLALCHEMY_DATABASE_URI'])
-if not engine.has_table("usuario"):
-    with app.app_context():
-        database.drop_all()
-        database.create_all()
-        print('Base de Dados Criada')
-else:
-    print('Base de Dados Existente')
+# from comunidadeimpressionadora import models
+# 
+# engine = sqlalchemy.create_engine(app.config['SQLALCHEMY_DATABASE_URI'])
+# 
+# if not engine.has_table("usuario"):
+#     with app.app_context():
+#         database.drop_all()
+#         database.create_all()
+#         print('Base de Dados Criada')
+# else:
+#     print('Base de Dados Existente')
 
 from comunidadeimpressionadora import routes
