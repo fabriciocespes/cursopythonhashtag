6e362f4d221562025aa1feb49d447075bb733a73
fabriciocespes <125491888+fabriciocespes@users.noreply.github.com>
2023-02-21 21:01:30 -0300

    Update __init__.py

--- comunidadeimpressionadora/__init__.py
+++ comunidadeimpressionadora/__init__.py
@@ -1,6 +1,6 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
-#from flask_bcrypt import Bcrypt
+from flask_bcrypt import Bcrypt
 from flask_login import LoginManager
 import os
 import sqlalchemy as sa
@@ -14,10 +14,10 @@ if os.getenv("DATABASE_URL"):
     app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv("DATABASE_URL")
 else:
     app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///comunidade.db'
-#app.config['SQLALCHEMY_DATABASE_URI'] = "postgresql://comunidade_dbj7_user:ud9ADfi1mCioBDCnNtvBg53fotcFntf5@dpg-cfoh9e14rebfdaopa3jg-a.oregon-postgres.render.com/comunidade_dbj7"
+# app.config['SQLALCHEMY_DATABASE_URI'] = "postgres://comunidade_dbj7_user:ud9ADfi1mCioBDCnNtvBg53fotcFntf5@dpg-cfoh9e14rebfdaopa3jg-a.oregon-postgres.render.com/comunidade_dbj7"
 
 database = SQLAlchemy(app)
-#bcrypt = Bcrypt(app)
+bcrypt = Bcrypt(app)
 login_manager = LoginManager(app)
 login_manager.login_view = 'login'
 login_manager.login_message_category = 'alert-info'
